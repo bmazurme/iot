e01f9e760eedb22012cf6c48aa6ecf564ae67b0c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-06-13 19:56:17 +0300

    fix: footer

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 
-import { BiLogoTelegram, BiLogoLinkedin } from '../../utils/icons/bi';
-import FooterMenu from '../footer-menu';
+// import FooterMenu from '../footer-menu';
 import { getCurrentYear } from '../../utils/get-current-year';
 
 import style from './footer.module.css';
 
 export default function Footer() {
   const getYear = getCurrentYear();
-  const links = [
-    {
-      icon: BiLogoTelegram,
-      label: 'Telegram',
-      url: 'https://t.me/ntlstl',
-    },
-    {
-      icon: BiLogoLinkedin,
-      label: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/bogdan-mazur-aba74287',
-    },
-  ];
 
   return (
     <footer className={style.footer}>
@@ -28,7 +15,7 @@ export default function Footer() {
         {` ${getYear} iot by `}
         <a className={style.link} href="https://ntlstl.dev/">ntlstl</a>
       </p>
-      <FooterMenu links={links} />
+      {/* <FooterMenu links={links} /> */}
     </footer>
   );
 }
